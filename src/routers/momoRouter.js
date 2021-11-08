15cc818ecf41a3e309b098d1b4a87b63753fd1f6
Haidang124 <haidangluu94@gmail.com>
2021-11-09 02:29:46 +0700

    momo payment

---
+++ src/routers/momoRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const momoController = require("../app/controllers/momoController");
+const authUser = require("../middleware/userMiddleware");
+//router
+// router.use(authUser);
+router.get("/payment", momoController.payment);
+module.exports = router;
